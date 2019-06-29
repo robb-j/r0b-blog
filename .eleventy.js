247b4b15a3bbd09385b84eac670ef182f56855f3
Rob Anderson <rob@andrsn.uk>
2019-06-29 23:39:09 +0100

    :art: Ran code through prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
-const { join } = require("path");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const { DateTime } = require("luxon");
+const { join } = require('path')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const { DateTime } = require('luxon')
 
-const unified = require("unified");
-const parseMarkdown = require("remark-parse");
-const stringify = require("remark-stringify");
-const remark2retext = require("remark-retext");
+// const unified = require('unified')
+// const parseMarkdown = require('remark-parse')
+// const stringify = require('remark-stringify')
+// const remark2retext = require('remark-retext')
 
-const excerpt = require("eleventy-plugin-excerpt");
-const readingTime = require("eleventy-plugin-reading-time");
+const excerpt = require('eleventy-plugin-excerpt')
+const readingTime = require('eleventy-plugin-reading-time')
 
 // const english = require('retext-english')
 // const assuming = require('retext-assuming')
@@ -37,45 +37,45 @@ const readingTime = require("eleventy-plugin-reading-time");
 //   )
 
 module.exports = function(config) {
-  config.addPlugin(pluginRss);
-  config.addPlugin(excerpt);
-  config.addPlugin(readingTime);
+  config.addPlugin(pluginRss)
+  config.addPlugin(excerpt)
+  config.addPlugin(readingTime)
 
-  config.addPassthroughCopy("node_modules/@robb_j/r0b-design/dist");
-  config.addPassthroughCopy("static");
-  config.addPassthroughCopy("static");
-  config.addPassthroughCopy("static");
+  config.addPassthroughCopy('node_modules/@robb_j/r0b-design/dist')
+  config.addPassthroughCopy('static')
+  config.addPassthroughCopy('static')
+  config.addPassthroughCopy('static')
 
   // Generate exerpts for pages
   // config.setFrontMatterParsingOptions({ excerpt: true });
 
-  config.addFilter("r0bAsset", value => {
-    if (!value) throw new Error("Invalid r0bAsset");
-    return join("/node_modules/@robb_j/r0b-design/dist", value);
-  });
+  config.addFilter('r0bAsset', value => {
+    if (!value) throw new Error('Invalid r0bAsset')
+    return join('/node_modules/@robb_j/r0b-design/dist', value)
+  })
 
-  config.addFilter("date", (value, format) => {
-    return DateTime.fromJSDate(value).toFormat(format);
-  });
+  config.addFilter('date', (value, format) => {
+    return DateTime.fromJSDate(value).toFormat(format)
+  })
 
-  config.addFilter("longDate", value =>
-    DateTime.fromJSDate(value).toFormat("cccc, d LLLL yyyy")
-  );
+  config.addFilter('longDate', value =>
+    DateTime.fromJSDate(value).toFormat('cccc, d LLLL yyyy')
+  )
 
-  config.addFilter("jsonDate", value => value.toISOString());
+  config.addFilter('jsonDate', value => value.toISOString())
 
-  config.addFilter("arrSlice", (value, begin, end) => value.slice(begin, end));
+  config.addFilter('arrSlice', (value, begin, end) => value.slice(begin, end))
 
   // Group posts into collections without tags
-  config.addCollection("posts", collection =>
-    collection.getFilteredByGlob("post/*.md")
-  );
+  config.addCollection('posts', collection =>
+    collection.getFilteredByGlob('post/*.md')
+  )
 
   // config.addPlugin(rssPlugin);
 
   return {
-    templateFormats: ["md", "njk"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk"
-  };
-};
+    templateFormats: ['md', 'njk'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk'
+  }
+}
