784b9fd9360be82b489abc690dfab59f5fe5e1bf
Rob Anderson <robanderson@hey.com>
2022-11-03 22:34:31 +0000

    add markdown anchors

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,27 @@
-const shortcodes = require('./11ty/shortcodes')
-const filters = require('./11ty/filters')
-const { PATH_PREFIX } = require('./11ty/env')
+require('dotenv/config')
+
+const markdown = require('markdown-it')
+const markdownAnchor = require('markdown-it-anchor')
 
 const pluginRss = require('@11ty/eleventy-plugin-rss')
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const readingTime = require('eleventy-plugin-reading-time')
 
+const shortcodes = require('./11ty/shortcodes')
+const filters = require('./11ty/filters')
+const { PATH_PREFIX } = require('./11ty/env')
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/js/')
+
+  const md = markdown({
+    html: true,
+    breaks: false,
+    linkify: false,
+  })
+  md.disable('code')
+  md.use(markdownAnchor)
+  eleventyConfig.setLibrary('md', md)
 
   eleventyConfig.addPassthroughCopy({
     'node_modules/@robb_j/r0b-design/dist': 'r0b',
