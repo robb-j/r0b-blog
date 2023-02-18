f957cc6232ef8d295693c5a3a999e83d1efb6e3b
Rob Anderson <robanderson@hey.com>
2023-02-18 17:40:59 +0000

    add "." command to edit blog posts

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,13 @@
-console.log('Hi')
+window.addEventListener('keyup', (event) => {
+  if (event.key !== '.') return
+  if (!('URLPattern' in window)) return
+
+  const pattern = new URLPattern({
+    pathname: '/post/:slug/',
+  })
+
+  const result = pattern.exec(location.href)
+  if (!result) return
+
+  location.href = `https://github.com/robb-j/r0b-blog/edit/main/content/post/${result.pathname.groups.slug}.md`
+})
